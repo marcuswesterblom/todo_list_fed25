24acdbc6f7e0ba6bf55597d464a59ca698304eaa
Marcus <marcus@Larss-MBP-2.lan>
2025-10-24 16:31:54 +0200

    Added Javascript, hard-coded list and styling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,54 @@
+import { Todo } from "./models/List";
 import "./style.css";
+
+const todos = [
+    new Todo("Städa"),
+    new Todo("Handla mat"),
+    new Todo("Gå ut med återvinning"),
+];
+
+    const containerListEx = document.getElementById("containerListEx");
+    const exampleList = document.createElement("ul");
+    const finishedList = document.createElement("ul");
+    exampleList.classList.add("list-disc");
+    finishedList.classList.add("list-disc");
+
+    const exampleListHeading = document.createElement("h3");
+    exampleListHeading.textContent = "Ex.";
+    exampleListHeading.classList.add("listExample");
+
+    containerListEx.appendChild(exampleListHeading);
+    containerListEx.appendChild(exampleList);
+
+    const finishedHeading = document.createElement("h3");
+    finishedHeading.textContent = "Klara";
+
+    const containerFinished = document.getElementById("containerFinished");
+    containerFinished.appendChild(finishedHeading);
+    containerFinished.appendChild(finishedList);
+
+todos.forEach((todo) => {
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox");
+
+    const text = document.createElement("p");
+    text.innerHTML = todo.text;
+
+    checkbox.addEventListener("change", () => {
+        todo.done = checkbox.checked;
+        if(todo.done){
+            finishedList.appendChild(li);
+            text.classList.add("line-through");
+        } else {
+            exampleList.appendChild(li);
+            text.classList.remove("line-through");
+        }
+    });
+
+    li.appendChild(text);
+    li.appendChild(checkbox);
+    exampleList.appendChild(li);
+});
---
+++ src/models/List.js
@@ -0,0 +1,8 @@
+export class Todo {
+    text;
+    done;
+    constructor(text, done){
+        this.text = text;
+        this.done = done;
+    }
+}
