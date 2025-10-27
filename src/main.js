64ef1157d800cfd848c4412a4f72a7e742ae10f2
Marcus <marcus@Larss-MBP-2.lan>
2025-10-27 13:01:41 +0100

    Updated code, style and clean. Form list Javascript

--- src/main.js
+++ src/main.js
@@ -1,54 +1,148 @@
+// class Todo - template for the todo-list
 import { Todo } from "./models/List";
 import "./style.css";
 
-const todos = [
-    new Todo("Städa"),
-    new Todo("Handla mat"),
-    new Todo("Gå ut med återvinning"),
-];
-
+// Exampel list
     const containerListEx = document.getElementById("containerListEx");
-    const exampleList = document.createElement("ul");
-    const finishedList = document.createElement("ul");
-    exampleList.classList.add("list-disc");
-    finishedList.classList.add("list-disc");
-
+        // Heading exampel list
     const exampleListHeading = document.createElement("h3");
     exampleListHeading.textContent = "Ex.";
     exampleListHeading.classList.add("listExample");
+        // ul
+    const exampleList = document.createElement("ul");
+    exampleList.classList.add("list-disc");
 
     containerListEx.appendChild(exampleListHeading);
     containerListEx.appendChild(exampleList);
 
-    const finishedHeading = document.createElement("h3");
-    finishedHeading.textContent = "Klara";
-
+// Finished List
     const containerFinished = document.getElementById("containerFinished");
-    containerFinished.appendChild(finishedHeading);
+        // ul
+    const finishedList = document.createElement("ul");
+    finishedList.classList.add("list-disc");
+
+        // Heading saying "Well done! You've finished all of your chores!" when all the boxes are checked
+    const youAreDoneText = document.createElement("h3");
+    youAreDoneText.textContent = "Well done! You've finished all of your chores!";
+    youAreDoneText.classList.add("youAreDone");
+
     containerFinished.appendChild(finishedList);
 
-todos.forEach((todo) => {
-    const li = document.createElement("li");
+// Hard-coded list
+    const todos = [
+    new Todo("Städa", false),
+    new Todo("Handla mat", false),
+    new Todo("Gå ut med återvinning", false),
+];
 
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList.add("checkbox");
+// forEach loop through hard-coded list
+    todos.forEach((todo) => {
+            // Create li
+        const li = document.createElement("li");
+            // Create input of the type "checkbox" added class for styling
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("checkbox");
+            // Create p element with the value from the hard-coded list
+        const text = document.createElement("p");
+        text.innerHTML = todo.text;
+        
+        li.appendChild(text);
+        li.appendChild(checkbox);
+        exampleList.appendChild(li);
+            // Eventlistener - When checkbox is checked li will be placed inside finished list otherwise in exampel-list
+        checkbox.addEventListener("change", () => {
+            todo.done = checkbox.checked;
+            if(todo.done){
+                finishedList.appendChild(li);
+                    // Tailwind styling
+                text.classList.add("line-through");
+            } else {
+                exampleList.appendChild(li);
+                    // Remove Tailwind styling
+                text.classList.remove("line-through");
 
-    const text = document.createElement("p");
-    text.innerHTML = todo.text;
-
-    checkbox.addEventListener("change", () => {
-        todo.done = checkbox.checked;
-        if(todo.done){
-            finishedList.appendChild(li);
-            text.classList.add("line-through");
-        } else {
-            exampleList.appendChild(li);
-            text.classList.remove("line-through");
-        }
+            }
+                // When there's more than 0 children (li) inside the finished ul(finishedList) containerFinished will have the class "show" (display:block). Otherwise it will remove the class "show" and go back to display: hidden
+                // containerFinished is only visible when finishedList contains li
+            if (finishedList.children.length > 0) {
+                containerFinished.classList.add("show");
+            } else {
+                containerFinished.classList.remove("show");
+            }
+            if(exampleList.children.length > 0) {
+                containerListEx.classList.remove("hide");
+                youAreDoneText.classList.remove("show");
+                containerFinished.classList.remove("green");
+            } else {
+                containerListEx.classList.add("hide");
+                youAreDoneText.classList.add("show");
+                containerFinished.appendChild(youAreDoneText);
+                containerFinished.classList.add("green");
+            }
+        });
     });
+    const containerListForm = document.getElementById("containerListForm");
+    const todoInput = document.getElementById("todoInput");
+    const todoBttn = document.getElementById("todoBttn");
+    const formList = document.createElement("ul");
+    formList.classList.add("list-disc");
 
-    li.appendChild(text);
-    li.appendChild(checkbox);
-    exampleList.appendChild(li);
-});
+    containerListForm.appendChild(formList);
+
+    function handleClick() {
+        const formLi = document.createElement("li");
+
+        const inputValue = document.createElement("p");
+        inputValue.textContent = todoInput.value;
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("checkbox");
+
+        checkbox.addEventListener("change", () => {
+            const checkboxChecked = checkbox.checked;
+
+            if(checkboxChecked){
+                finishedList.appendChild(formLi);
+                    // Tailwind styling
+                inputValue.classList.add("line-through");
+            } else {
+                formList.appendChild(formLi);
+                    // Remove Tailwind styling
+                inputValue.classList.remove("line-through");
+            }
+
+            if(finishedList.children.length > 0){
+                containerFinished.classList.add("show");
+            } else {
+                containerFinished.classList.remove("show");
+            }
+            if(formList.children.length === 0) {
+                containerListForm.classList.remove("show");
+            } else {
+                containerListForm.classList.add("show");
+            }
+            if(formList.children.length === 0) {
+            youAreDoneText.classList.add("show");
+            containerFinished.appendChild(youAreDoneText);
+            containerFinished.classList.add("green");
+            } else {
+            youAreDoneText.classList.remove("show");
+            containerFinished.classList.remove("green");
+            }
+        });
+
+        formList.appendChild(formLi);
+        formLi.appendChild(inputValue);
+        formLi.appendChild(checkbox);
+
+        if(formList.children.length > 0){
+            containerListForm.classList.add("show");
+            formList.appendChild(formLi);
+        } else {
+            containerListForm.classList.remove("show");
+        }
+    }
+
+    todoBttn.addEventListener("click", handleClick);
